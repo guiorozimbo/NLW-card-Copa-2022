c0831aa58e01465292a7414d834b42c1836e07b1
Guilherme <guilhermegv890@gmail.com>
2022-11-06 19:56:10 -0300

    NLW card Cup 2022 rockeatseat

---
+++ script.js
@@ -0,0 +1,57 @@
+let isIgnite = false
+function changeCard(event) {
+  const card = event.currentTarget
+  const bg = isIgnite ? "explorer" : "ignite"
+  isIgnite = !isIgnite
+  card.style.backgroundImage = `url(./assets/bg-${bg}.svg)`
+}
+
+let isIgniteCard = false;
+const shape = document.getElementById("shape");
+
+var count = 200;
+var defaults = {
+  origin: { y: 0.7 }
+};
+
+function fire(particleRatio, opts) {
+  confetti(Object.assign({}, defaults, opts, {
+    particleCount: Math.floor(count * particleRatio)
+  }));
+}
+
+function triggerFire(){
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+  fire(0.2, {
+    spread: 60,
+  });
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+}
+
+setTimeout(triggerFire, 1000);
+
+function switchCard(event) {
+
+  const card = event.currentTarget;
+  const backgroundImage = isIgniteCard ? "ignite" : "explorer";
+  isIgniteCard = !isIgniteCard;
+  card.style.backgroundImage = `url(./assets/bg-${backgroundImage}.svg)`;
+  shape.style.backgroundImage = `url(./assets/shape-${backgroundImage}.svg)`;
+}
